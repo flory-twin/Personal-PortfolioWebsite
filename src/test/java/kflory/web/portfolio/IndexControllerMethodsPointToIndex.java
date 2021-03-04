c7465fed1100f22d01de1e83e084856e67a7322c
flory.twin <flory.twin.2@gmail.com>
2021-03-04 15:11:42 -0500

    Multiple small changes to make existing project work.
    1. Suddenly started having issues with being unable to find views. Per https://stackoverflow.com/questions/36100212/by-default-where-does-spring-boot-expect-views-to-be-stored, updated default page type to html and moved it out of WEB-INF/views/index to /views.
    2. Adds tests on index page.
    3. Note that the uncontrolled project
    files somehow became corrupted and the entire project had to be rebuilt,
    which has led to minor changes in files normally controlled by the
    Spring Initializr generator or by Eclipse. In addition, since
    regeneration was via the Spring Initializr, I took the opportunity to
    upgrade Spring Boot to 2.4.3.

---
+++ src/test/java/kflory/web/portfolio/IndexControllerMethodsPointToIndex.java
@@ -0,0 +1,53 @@
+package kflory.web.portfolio;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.URI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+//If annotation not present, fails; @Test MUST be used with @SpringBootTest unless one wants a standard JUnit test
+// Note that this annotation "tells Spring Boot to look for a main configuration class (one with @SpringBootApplication, for instance) and use that to start a Spring application context." Not sure whether a context would be provided and/or any Spring testing infrastructure would pick up tests otherwise...
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class IndexControllerMethodsPointToIndex {
+	
+	@LocalServerPort
+	private int port;
+	
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
+	@Test
+	public void testGetReturnsNonNull() throws Exception {
+		assertThat(
+			this.restTemplate.getForObject(
+				"http://localhost:" + port + "/",
+				String.class))
+			.isNotNull();
+	}
+	
+	@Test
+	public void testGetReturnsDefaultMessage() throws Exception {
+		assertThat(
+			this.restTemplate.getForObject(
+				"http://localhost:" + port + "/",
+				String.class))
+			.contains("Hail");
+	}
+	
+	@Test
+	public void testPostReturnsDefaultMessage() throws Exception {
+		assertThat(
+			this.restTemplate.postForObject(
+				"http://localhost:" + port + "/",
+				new String("Fail! This string will be in the request body."),
+				String.class))
+			.contains("failsnail");
+	}
+}
